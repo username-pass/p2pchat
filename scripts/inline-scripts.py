c461a393559aaded59b7878fd5b7b9a91843321f
[Kuplynx] <92491671+Kuplynx@users.noreply.github.com>
2024-01-31 20:34:06 -0800

    hopefully finish workflows

--- scripts/inline-scripts.py
+++ scripts/inline-scripts.py
@@ -5,20 +5,20 @@ import sys
 
 html_file_path = sys.argv[1]
 
-with open(html_file_path, 'r') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+with open(html_file_path, "r") as file:
+    soup = BeautifulSoup(file, "html.parser")
 
-for script_tag in soup.find_all('script', src=True):
-    script_src = script_tag['src']
+for script_tag in soup.find_all("script", src=True):
+    script_src = script_tag["src"]
     script_content = ""
     if (script_src.startswith("./")):
         script_src = script_src[2:]
-        with open(script_src, 'r') as file:
+        with open(script_src, "r") as file:
             script_content = file.read()
     else:
         script_content = requests.get(script_src).text  # Use requests to fetch script content, adjust as needed
     script_tag.string = script_content
 
 # Save the modified HTML to a new file
-with open('path/to/inlined-file.html', 'w') as new_file:
+with open("inlined.html", "w+") as new_file:
     new_file.write(str(soup))
