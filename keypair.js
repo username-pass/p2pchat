dd6aa7d3f6c2019ddefac2ae4db1c303de7f5800
[Kuplynx] <92491671+Kuplynx@users.noreply.github.com>
2024-01-31 20:19:07 -0800

    init

---
+++ keypair.js
@@ -0,0 +1,153 @@
+class KeyPair {
+    constructor() {
+      this.publicKey = null;
+      this.privateKey = null;
+    }
+
+    async generateKeyPair() {
+      try {
+        const keyPair = await window.crypto.subtle.generateKey(
+          {
+            name: "RSA-OAEP",
+            modulusLength: 4096,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: "SHA-256",
+          },
+          true,
+          ["encrypt", "decrypt"]
+        );
+
+        this.publicKey = await window.crypto.subtle.exportKey(
+          "spki",
+          keyPair.publicKey
+        );
+        this.privateKey = await window.crypto.subtle.exportKey(
+          "pkcs8",
+          keyPair.privateKey
+        );
+      } catch (error) {
+        console.error("Error generating key pair:", error);
+      }
+    }
+    async encrypt(data) {
+      try {
+        const publicKey = await window.crypto.subtle.importKey(
+          "spki",
+          this.publicKey,
+          {
+            name: "RSA-OAEP",
+            hash: "SHA-256",
+          },
+          true,
+          ["encrypt"]
+        );
+
+        const ciphertext = await window.crypto.subtle.encrypt(
+          {
+            name: "RSA-OAEP",
+          },
+          publicKey,
+          new TextEncoder().encode(data)
+        );
+
+        return KeyPair.arrayBufferToHex(ciphertext);
+      } catch (error) {
+        console.error("Error encrypting data:", error);
+      }
+    }
+    static async encryptWithPublicKey(publicKey, data) {
+      try {
+        const importedPublicKey = await window.crypto.subtle.importKey(
+          "spki",
+          KeyPair.hexToArrayBuffer(publicKey),
+          {
+            name: "RSA-OAEP",
+            hash: "SHA-256",
+          },
+          true,
+          ["encrypt"]
+        );
+
+        const ciphertext = await window.crypto.subtle.encrypt(
+          {
+            name: "RSA-OAEP",
+          },
+          importedPublicKey,
+          new TextEncoder().encode(data)
+        );
+
+        return KeyPair.arrayBufferToHex(ciphertext);
+      } catch (error) {
+        console.error("Error encrypting data with public key:", error);
+      }
+    }
+
+    async decrypt(encryptedData) {
+      try {
+        const privateKey = await window.crypto.subtle.importKey(
+          "pkcs8",
+          this.privateKey,
+          {
+            name: "RSA-OAEP",
+            hash: "SHA-256",
+          },
+          true,
+          ["decrypt"]
+        );
+
+        const decryptedData = await window.crypto.subtle.decrypt(
+          {
+            name: "RSA-OAEP",
+          },
+          privateKey,
+          KeyPair.hexToArrayBuffer(encryptedData)
+        );
+
+        return new TextDecoder().decode(decryptedData);
+      } catch (error) {
+        console.error("Error decrypting data:", error);
+      }
+    }
+
+    exportHexKeys() {
+      return {
+        publicKey: KeyPair.arrayBufferToHex(this.publicKey),
+        privateKey: KeyPair.arrayBufferToHex(this.privateKey)
+      };
+    }
+
+    importHexKeys(hexPublicKey, hexPrivateKey) {
+      this.publicKey = KeyPair.hexToArrayBuffer(hexPublicKey);
+      this.privateKey = KeyPair.hexToArrayBuffer(hexPrivateKey);
+    }
+
+    static bufferToHex(buffer) {
+      return Array.from(new Uint8Array(buffer))
+        .map((byte) => byte.toString(16).padStart(2, "0"))
+        .join("");
+    }
+
+    static arrayBufferToHex(buffer) {
+      const view = new DataView(buffer);
+      let hexString = "";
+
+      for (let i = 0; i < view.byteLength; i++) {
+        const byte = view.getUint8(i);
+        hexString += byte.toString(16).padStart(2, "0");
+      }
+
+      return hexString;
+    }
+
+    static hexToArrayBuffer(hexString) {
+      const buffer = new ArrayBuffer(hexString.length / 2);
+      const view = new DataView(buffer);
+
+      for (let i = 0; i < hexString.length; i += 2) {
+        const byte = parseInt(hexString.substr(i, 2), 16);
+        view.setUint8(i / 2, byte);
+      }
+
+      return buffer;
+    }
+  }
---
+++ p2chat.js
@@ -0,0 +1,136 @@
+class P2chat {
+    constructor(fs) {
+        this.fs = fs;
+        this.peer;
+        this.conn;
+        this.keyPair = {
+            "public": "---",
+            "private": "---"
+        };
+        
+        this.id;
+    }
+    init() {
+        this.peer = new Peer();
+        this.peer.on('open', (id) => {
+          //display id
+          document.getElementById('your-id').value = id;
+          this.id = id;
+        });
+        this.peer.on('connection', (connection) => {
+          console.log(typeof this, this);
+          this.conn = connection;
+          this.setConnectionIndicator(true);
+          this.displayMessage("status", "", 'connected to: ' + this.conn.peer);
+          this.conn.on('data', this.doData);
+        });
+        this.peer.on('disconnected', () => {
+          this.setConnectionIndicator(false);
+        })
+        this.peer.on('close', () => {
+          this.setConnectionIndicator(false);
+        });
+        this.peer.on('error', (error) => {
+          this.displayMessage('error', "", error)
+          this.setConnectionIndicator(false);
+        });
+        document.getElementById('other-id').addEventListener('input', function(event) {
+          var otherPeerId = event.target.value;
+          if (otherPeerId) {
+              console.log(this);
+              let peer = p2chat.peer;
+              console.log(peer);
+            p2chat.conn = peer.connect(otherPeerId);
+            p2chat.conn.on('open', function() {
+              p2chat.displayMessage("status", "", 'Connection established with ' + otherPeerId);
+            });
+            p2chat.conn.on('data', function(data) {
+              p2chat.doData(data)
+            });
+          }
+        });
+    }
+    resetPeerId(newId) {
+      this.peer.destroy();
+      this.peer = new Peer(newId);
+    }
+
+    copyIdToClipboard() {
+      let copyText = document.getElementById('your-id');
+      copyText.select();
+      document.execCommand('copy');
+      alert('ID copied to clipboard!');
+    }
+
+    sendMessage(e) {
+      if (e.key != "Enter") return;
+      let message = document.getElementById('message').value;
+      if (p2chat.conn && message) {
+        p2chat.sendData({
+          type: "message",
+          ok: true,
+          status: "200 message sent",
+          message: message
+        })
+        p2chat.displayMessage("message", "you", message);
+        document.getElementById('message').value = '';
+      }
+    }
+
+    sendData(data) {
+      if (!this.conn) return;
+      let toSend = data || statuses["500"];
+      toSend.sender = this.sender;
+      this.conn.send(toSend);
+
+    }
+
+    doData(data) {
+      console.log("received data", data);
+      console.log(this);
+      console.log(p2chat);
+      console.log(P2chat);
+      if (typeof data == "String") {
+        p2chat.displayMessage("data", "unkown", data);
+        return;
+      }
+      switch (data.type) {
+        case "command":
+          p2chat.execCommand(data.command);
+          break;
+        default:
+          p2chat.displayMesssage(data.type, data.sender, data[data.type]);
+          break;
+      }
+    }
+
+    displayMessage(type = "data", sender = "unknown", data = "") {
+      let chatHistory = document.getElementById("chat-history");
+      let messageContainer = document.createElement("div");
+      messageContainer.classList.add("message-element-container");
+      let senderElement = document.createElement("span");
+      senderElement.classList.add("message-sender-element");
+      senderElement.textContent = sender.senderString;
+      let messageTypeElement = document.createElement("span");
+      messageTypeElement.classList.add("message-type-element");
+      messageTypeElement.textContent = type;
+      let messageElement = document.createElement("span");
+      messageElement.classList.add("message-element");
+      messageElement.textContent = data;
+      messageElement.appendChild(messageTypeElement);
+      messageElement.appendChild(senderElement);
+      messageContainer.appendChild(messageElement);
+      chatHistory.appendChild(messageContainer);
+    }
+
+
+    setConnectionIndicator(status) {
+      document.getElementById("connection-indicator").checked = status;
+    }
+    login(id = this.id) {
+        
+    }
+    createKeyPair() {
+        
+    }
+}
