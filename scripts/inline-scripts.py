ab3d423244f91e5710442c0667d2fb20c86cd0bb
[Kuplynx] <92491671+Kuplynx@users.noreply.github.com>
2024-01-31 20:31:05 -0800

    inliner

---
+++ scripts/inline-scripts.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import sys
+
+html_file_path = sys.argv[1]
+
+with open(html_file_path, 'r') as file:
+    soup = BeautifulSoup(file, 'html.parser')
+
+for script_tag in soup.find_all('script', src=True):
+    script_src = script_tag['src']
+    script_content = ""
+    if (script_src.startswith("./")):
+        script_src = script_src[2:]
+        with open(script_src, 'r') as file:
+            script_content = file.read()
+    else:
+        script_content = requests.get(script_src).text  # Use requests to fetch script content, adjust as needed
+    script_tag.string = script_content
+
+# Save the modified HTML to a new file
+with open('path/to/inlined-file.html', 'w') as new_file:
+    new_file.write(str(soup))
