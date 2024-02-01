ea33308a980043adb07d7886f89f632fcc124b61
[Kuplynx] <92491671+Kuplynx@users.noreply.github.com>
2024-01-31 20:45:46 -0800

    remove src tags

--- scripts/inline-scripts.py
+++ scripts/inline-scripts.py
@@ -18,6 +18,7 @@ for script_tag in soup.find_all("script", src=True):
     else:
         script_content = requests.get(script_src).text  # Use requests to fetch script content, adjust as needed
     script_tag.string = script_content
+    script_src = ""
 
 # Save the modified HTML to a new file
 with open("inlined.html", "w+") as new_file:
